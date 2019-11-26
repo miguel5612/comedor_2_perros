4049207bf57c4ba4c287a9d0574f0dbcd193e44c
miguel califa <miguelangelcu@ufps.edu.co>
2019-11-26 18:06:59 -0500

    Commti test v1

--- comedor_2_perros.ino
+++ comedor_2_perros.ino
@@ -6,29 +6,35 @@
 
 Servo servoMotor;
 
-#define SS_RF 10
-#define SS_RFID 8
+#define SS_RF 9
+#define SS_RFID 10
 
-#define RST_RFID 6
-
-Nrf24 radiorf24(SS_RF,9);
-Rfid rfid(7,SS_RFID);
+//#define RST_RFID 6 -> Este cable ahora va a tierra.
+Nrf24 radiorf24(SS_RF,109); //9
+Rfid rfid(107,SS_RFID); //7
 
 int perros[2][4] = { {154,113,138,22} , {58,193,190,21} };
 
-
 void setup() { 
-   
-  pinMode (SS_RF, OUTPUT);
-  pinMode (SS_RFID, OUTPUT);
-  
-  digitalWrite(SS_RF, HIGH);
-  digitalWrite(SS_RFID, HIGH);
 
-  Serial.begin(9600); 
-  SPI.begin();
-  radiorf24.begin();
-  rfid.begin();
+  //Configuracion de entradas y salidas
+  pinMode (SS_RF, OUTPUT); //Declaro las variables como salidas
+  pinMode (SS_RFID, OUTPUT); //Declaro las variables como salidas
+
+  //Deshabilito ambos para que ninguno hable.
+  digitalWrite(SS_RF, HIGH); //Con esto deshabilito el RF - Logica invertida
+  digitalWrite(SS_RFID, LOW); // Con esto deshabilito el RFid - Logica normal
+
+  // Inicializacion de los protocolos de comunicacion
+  Serial.begin(115200); //Inicializo la comunicacion serie a 115200 baudios
+  SPI.begin(); // Inicializo la comunicacion SPI
+
+  // Inicializo los modulos de comunicaciones
+  Serial.println("Inicializando RF24");enableRF();radiorf24.begin(); 
+  Serial.println("Inicializando RFid");enableRFID();rfid.begin();
+
+  //Mensaje de salida exitosa del setup
+  Serial.println("He finalizado con exito las configuraciones");
 }
 
 void loop() {
@@ -37,14 +43,14 @@ void loop() {
 
   if(perro_rf_index >= 0){
     Serial.print("Se recibio RF el perro: ");
-    Serial.println(perro_rf_index);
+    Serial.println(perro_rf_index + 1);
   }
   enableRFID();
   int perro_index = validar(rfid.read()); 
    
   if(perro_index >= 0){
     Serial.print("Se lee el perro: ");   
-    Serial.println(perro_index);     
+    Serial.println(perro_index + 1);     
     int perro[4] = { perros[perro_index][0], perros[perro_index][1], perros[perro_index][2],perros[perro_index][3]};   
     
     enableRF();
@@ -64,9 +70,13 @@ int validar( int * lectura){
 }
 
 void enableRFID(){
+  digitalWrite(SS_RF, HIGH); //Con esto deshabilito el RF - Logica invertida
+  digitalWrite(SS_RFID, HIGH); // Con esto habilito el RFid - Logica normal
 }
 
 void enableRF(){
+  digitalWrite(SS_RF, LOW); //Con esto habilito el RF - Logica invertida
+  digitalWrite(SS_RFID, HIGH); // Con esto deshabilito el RFid - Logica normal
 }
 
 void abrir(){
